01582d719ab125a02479474083d43a226cc61d2b
rafael-men <rafaelgoncalves1822@gmail.com>
2025-03-05 13:37:30 -0300

    categories

--- backend/src/controllers/category/category.controller.ts
+++ backend/src/controllers/category/category.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateCategoryDto } from 'src/dto/create-category.dto';
+import { CategoryService } from 'src/services/category/category.service';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+    constructor (private readonly categoryService:CategoryService) {}
+
+    @Post('new')
+    create(@Body() createCategoryDto : CreateCategoryDto ) {
+        return this.categoryService.saveCategory(createCategoryDto);
+    }
+
+    @Get()
+    getAllCategories(@Body() createCategoryDto:CreateCategoryDto) {
+        return this.categoryService.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param('id') id:string) {
+        return this.categoryService.findById(id);
+    }
+
+    @Put('atualizar/:id')
+    updateById(@Param('id') id:string, @Body() updateCategoryDto:CreateCategoryDto) {
+        try {
+            return this.categoryService.updateById(id,updateCategoryDto);
+        }
+        catch(error) {
+            throw new BadRequestException('Erro inesperado, tente novamente');
+        }
+    }
+
+
+    @Delete('deletar/:id')
+    deleteCategory(@Param('id') id:string) {
+        try {
+            return this.categoryService.deleteCategory(id);
+        }
+        catch(error) {
+            throw new BadRequestException('Erro inesperado, tente novamente.')
+        }
+    }
+}
--- backend/src/services/category/category.service.ts
+++ backend/src/services/category/category.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { error } from 'console';
+import { Model } from 'mongoose';
+import { CreateCategoryDto } from 'src/dto/create-category.dto';
+import { Category } from 'src/models/Category/category.schema';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(@InjectModel(Category.name) private categoryModel: Model<Category>) {}
+
+    async saveCategory(createCategoryDto:CreateCategoryDto):Promise<Category> {
+        try {
+            const categoryCreated = new this.categoryModel(createCategoryDto);
+            return categoryCreated.save();
+        }
+        catch(err) {
+            throw new error('Erro ao salvar a categoria.', err);
+        }
+    }
+
+    async findAll(): Promise<Category[]> {
+        try {
+            return this.categoryModel.find().exec();
+        }
+        catch(err) {
+            throw new error('Erro ao buscar as categorias.',err)
+        }
+    }
+
+    async findById(id: string): Promise<Category> {
+        try {
+            const category = await this.categoryModel.findById(id).exec();
+            if(!category) {
+                throw new NotFoundException('Categoria não encontrada.');
+            }
+            return category;
+        }
+        catch(err) {
+            throw new error('Erro ao buscar a categoria.',err);
+        }
+    }
+
+    async updateById(id:string,updateCategoryDto:CreateCategoryDto): Promise<Category> {
+        try {
+            const updated = await this.categoryModel.findByIdAndUpdate(id,updateCategoryDto,{new:true}).exec();
+            if(!updated) {
+                throw new NotFoundException('Categoria não encontrada, não foi possível atualizar.')
+            }
+            return updated;
+        }
+        catch(err) {
+            throw new error('Não foi possível atualizar a categoria, erro inesperado.',err);
+        }
+    }
+
+    async deleteCategory(id:string):Promise<Category> {
+        try {
+            const deleted = await this.categoryModel.findByIdAndDelete(id).exec();
+            if(!deleted) {
+                throw new NotFoundException('Categoria não encontrada.');
+            }
+            return deleted;
+        }
+        catch(err) {
+            throw new error('Erro ao buscar a categoria.',err);
+        }
+    }
+ }
